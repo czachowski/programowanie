e4528f63788f9573f78815a802553ff39ce56b68
Szymon <32205986+czachowski@users.noreply.github.com>
2017-10-17 20:41:31 +0200

    new

--- angular/projekty/new-single-page-app/src/app/app.component.ts
+++ angular/projekty/new-single-page-app/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items = ["Angular 4", "React", "Underscore"];
+  newItem = "";
+  pushItem = function() {
+  	if(this.newItem != "") {
+  		this.items.push(this.newItem);
+  		this.newItem = "";
+  	}
+  };
+  removeItem = function(index) {
+  	this.items.splice(index, 1);
+  }
 }
