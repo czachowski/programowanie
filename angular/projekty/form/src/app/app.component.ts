0748cccba8cda264c2d9e141a0603228f28de9e0
Szymon <32205986+czachowski@users.noreply.github.com>
2017-10-23 15:53:56 +0200

    nowy proj

--- angular/projekty/form/src/app/app.component.ts
+++ angular/projekty/form/src/app/app.component.ts
@@ -27,5 +27,50 @@ decimalChanged = function (oldValue, newValue) {
       this.form.patchValue({octal: ""});
       this.form.patchValue({hexal: ""});
     }
+  };
+
+b=0;
+
+o=0;
+
+h=0;
+
+binaryChanged = function (oldValue, newValue) {
+  this.b = this.b + 1;
+    if(this.b == 1) {
+      if (newValue != "") {
+        this.form.patchValue({decimal: parseInt(newValue, 2).toString(10)});
+      } else {
+        this.form.patchValue({decimal: ""});
+      }
+      this.b = 0;
+    }
   }
+
+octalChanged = function (oldValue, newValue) {
+  this.o = this.o + 1;
+    if(this.o == 1) {
+      if (newValue != "") {
+        this.form.patchValue({decimal: parseInt(newValue, 8).toString(10)});
+      } else {
+        this.form.patchValue({decimal: ""});
+      }
+      this.o = 0;
+    }
+  }
+
+hexalChanged = function (oldValue, newValue) {
+  this.h = this.h + 1;
+    if(this.h == 1) {
+      if (newValue != "") {
+        this.form.patchValue({decimal: parseInt(newValue, 16).toString(10)});
+      } else {
+        this.form.patchValue({decimal: ""});
+      }
+      this.h = 0;
+    }
+  }
+
+
+
 }
