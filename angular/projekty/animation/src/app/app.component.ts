ece413e3994991d6d8a35ba6c24f605b5f5fc1af
Szymon <32205986+czachowski@users.noreply.github.com>
2017-10-25 19:09:53 +0200

    new proj

---
+++ angular/projekty/animation/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  form;
+  ngOnInit (){
+  	this.form = new FormGroup ({
+  		firstname: new FormControl('', Validators.compose([
+  			Validators.required,
+  			Validators.minLength(3),
+  			Validators.pattern('[\\w\\-\\s\\/]+')
+  			])),
+  		lastname: new FormControl('', this.textValidators),
+  		language: new FormControl(''),
+  	});
+  }
+
+  textValidators(control){
+  	if(control.value.length < 3) {
+  		return {'lastname': true};
+  	}
+  }
+
+  onSubmit = function(user){
+  	console.log(user);
+  }
+}
